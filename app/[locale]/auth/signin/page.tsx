13979c3c29d76ad096b8b990874598b0ea31e397
GabrielRodrigues54 <gabttorodrigues54@gmail.com>
2025-07-29 20:25:37 -0300

    progressbar

--- app/[locale]/auth/signin/page.tsx
+++ app/[locale]/auth/signin/page.tsx
@@ -2,6 +2,7 @@ import { SignInPage } from "@/components/Pages/Auth/SignIn";
 import { getTranslations } from "next-intl/server";
 import { Metadata } from "next";
 
+
 // Transforma o metadata em uma função assíncrona
 export async function generateMetadata({params: {locale}}: {params: {locale: string}}): Promise<Metadata> {
   const t = await getTranslations({locale, namespace: 'SignInPage'});
